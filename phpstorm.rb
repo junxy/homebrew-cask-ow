e08c973c34ea2a9fb4bf5048cd1a3ded5c9327fe
Junior Hsu <ayijun@gmail.com>
2014-07-12 16:57:40 +0800

    init

---
+++ phpstorm.rb
@@ -0,0 +1,10 @@
+class Phpstorm < Cask
+  version '7.1.3'
+  sha256 '37ab8cb0739388da77d226fbdbf956ef441a8d7cb59e316e22815d57d11f6a1d'
+
+#  url 'http://download.jetbrains.com/webide/PhpStorm-7.1.3.dmg'
+  url 'http://dev-download.oceanwing.com/PhpStorm-7.1.3.dmg'
+  homepage 'http://www.jetbrains.com/phpstorm/'
+
+  link 'PhpStorm.app'
+end
